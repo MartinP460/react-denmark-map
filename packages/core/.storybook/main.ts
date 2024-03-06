ba2d2c73e05a76c399061af78ef0971000d35a6b
Martin Polley <martin.polley@lenus.io>
2024-03-06 17:14:42 +0100

    Fix Storybook config.

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5'
+import path from 'path'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +15,14 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag'
+  },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src')
+    }
+
+    return config
   }
 }
 export default config
