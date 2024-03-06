45db4f0e3f5db19c0e29a65a1844a807c71927ec
Martin Polley <martin.polley@lenus.io>
2024-03-06 11:00:46 +0100

    Remove Jest config option.

--- packages/core/jest.config.cjs
+++ packages/core/jest.config.cjs
@@ -3,6 +3,5 @@ module.exports = {
   moduleNameMapper: {
     '.(css|less|scss)$': 'identity-obj-proxy',
     '@/(.*)': ['<rootDir>/src/$1']
-  },
-  moduleDirectories: ['node_modules', 'src']
+  }
 }
