ec5d9b77565787182bcd6ef5b3d5d959a7355c4c
Martin Polley <martin.polley460@gmail.com>
2025-04-16 17:35:15 +0200

    Fix tests.

--- packages/core/jest.config.cjs
+++ packages/core/jest.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
   moduleNameMapper: {
     '.(css|less|scss)$': 'identity-obj-proxy',
     '@/(.*)': ['<rootDir>/src/$1'],
-    '^react$': '<rootDir>/node_modules/react/index.js',
+    '^react$': '<rootDir>/../../node_modules/react/index.js',
   }
 }
