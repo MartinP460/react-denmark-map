17be5696470f7b94fd8693a70f1553ee96164f0d
Martin Polley <martin.polley460@gmail.com>
2025-04-16 15:43:22 +0200

    Upgrade React and Storybook in core.

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -3,19 +3,23 @@ import path, { dirname, join } from 'path'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@storybook/addon-interactions')
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath("@storybook/addon-webpack5-compiler-babel"),
+    '@chromatic-com/storybook'
   ],
+
   framework: {
-    name: getAbsolutePath('@storybook/react-webpack5'),
+    name: getAbsolutePath("@storybook/nextjs"),
     options: {}
   },
-  docs: {
-    autodocs: 'tag'
-  },
+
+  docs: {},
+
   webpackFinal: async (config) => ({
     ...config,
     resolve: {
@@ -25,7 +29,11 @@ const config: StorybookConfig = {
         '@': path.resolve(__dirname, '../src')
       }
     }
-  })
+  }),
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 }
 export default config
 
